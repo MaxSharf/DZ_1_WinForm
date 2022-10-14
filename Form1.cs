d6b4001c04fceba261285b136abaecdf16d75b12
Sara_xy66@student.itstep.org <Sara_xy66@student.itstep.org>
2022-10-14 16:31:53 +0300

    Виконано

--- Form1.cs
+++ Form1.cs
@@ -15,8 +15,23 @@ namespace DZ_1_WinForm
         public Form1()
         {
             InitializeComponent();
+            this.Shown += ShowMesBox;
+
         }
 
-       
+        public void ShowMesBox(Object sender, EventArgs e)
+        {
+            string[] array = { "Hi my name is Max , i am from Zhmerinka , stydent of VTFK", "Windows Forms", "Grup: БВ111" };
+            int numSymbol = 0;
+            string caption;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                numSymbol += array[i].Length;
+                caption = (array.Length - 1 == i) ? $"MessageBox {i + 1}. Число символів- {numSymbol / array.Length}" : $"MessageBox {i + 1}";
+                MessageBox.Show(array[i], caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
     }
 }
