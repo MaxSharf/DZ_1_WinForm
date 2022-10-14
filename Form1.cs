e5cddfbaf5685e5bff4bd3c2c9be61b63ef99f08
Sara_xy66@student.itstep.org <Sara_xy66@student.itstep.org>
2022-10-14 16:39:29 +0300

    Виконанно

--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,27 @@ namespace DZ_1_WinForm
         public Form1()
         {
             InitializeComponent();
+            this.Shown += Start;
         }
-    }
+
+        public void Start(object sender, EventArgs e)
+        {
+            DialogResult result;
+            int numDialog = 1;
+            while (true)
+            {
+                result = MessageBox.Show($"{new Random().Next(1, 2000)}", "Загадайте число від 1 до 2000", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    MessageBox.Show($"Кільлість запитів{numDialog}", "Успіх", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    numDialog = 0;
+                    result = MessageBox.Show($"Продовжити ?", "Вихід", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (result == DialogResult.No) this.Close();
+                }
+                numDialog++;
+            }
+            }
+
+
+        }
 }
